ea46eaf6494cecd910fe25e1b9c2452149afaf8a
seliinatmaca <selinatmacayy@gmail.com>
2024-11-09 21:23:04 +0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import {View, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+import Routes from './src/navigation/Routes';
+
+const App = () => {
+  return <Routes />;
+};
+
+export default App;
